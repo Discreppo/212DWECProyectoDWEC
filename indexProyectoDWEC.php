0e63c6f940060b943d81faf31f0bb8a2ed495ff8
Discreppo <discreppodedaw@gmail.com>
2023-11-17 09:30:04 +0100

    Version_1.0

---
+++ indexProyectoDWEC.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oscar Pascual Ferrero</title>
+    <style>
+        
+        body {            
+            display: flex;
+            flex-direction: column;              
+        }
+        footer{
+            bottom: 0;
+            position: fixed;
+            text-align: center;
+            width: 100%;
+            background-color: lightgray;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Desarrollo web entorno cliente</h1>
+    </header>
+
+    <main>
+        <!-- Contenido principal de tu sitio web -->
+        <p>TEMA 1:</p>
+        <a href="doc/ejerciciosPracticosTema1.pdf" target="_blank">Tema 1</a>
+        <p>TEMA 2:</p>
+        <a href="doc/practica2.1.pdf" target="_blank">Practica2</a>
+        <a href="doc/practica2.html" target="_blank">HTML</a>
+        <p>TEMA 3:<a href="../212DWESProyectoTema3/indexProyectoTema3.php"></a></p>
+        
+    </main>
+
+    <footer>
+        <p><a href="../index.html">Oscar Pascual Ferrero</a></p>
+        <p><a href="indexProyectoDWEC.php">Inicio</a> | <a href="https://github.com/discreppo" target="_blank" >GitHub</a></p>
+    </footer>
+</body>
+</html>
